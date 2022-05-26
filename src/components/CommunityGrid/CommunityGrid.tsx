aabbc801adfd4ec0ee81afdb6fba2ad4c9fa6411
snowwhitedev <snowwhite91924@gmail.com>
2022-05-27 04:45:12 +0700

    feat: community page

--- src/components/CommunityGrid/CommunityGrid.tsx
+++ src/components/CommunityGrid/CommunityGrid.tsx
@@ -5,105 +5,7 @@ import CommunityModal from './CommunityModal'
 import styles from './CommunityGrid.module.scss'
 import { gsap, Power1 } from "gsap";
 import { Flip } from "gsap/dist/Flip";
-
-const contents = [
-  {
-    id: '0',
-    title: "UPDATES",
-    numeration: "01",
-    label: "Keep up on the latest in the Red Cat MUltiverse",
-    column: "column_1",
-    column_m: "column_1",
-    item: 'item_a',
-    text: "",
-  },
-  {
-    id: '1',
-    title: "WHITEPAPER",
-    numeration: "07",
-    label: "Want to know more about what we’re doing?",
-    column: "column_1",
-    column_m: "column_1",
-    item: 'item_b',
-    text: ["It all started with one question: What if we could harness the engagement potential of blockchain gaming, but also encourage people to learn skills that go beyond playing games and make them more employable?", "In the Summer of 2021, we merged our 2019 research findings on soft skills with blockchain gaming and this is how the RedCat Multiverse was born with the world’s first Play-Learn-Earn model.", "RedCat Multiverse is a highly engaging blockchain game that provides socio - emotional activities, building skill mastery and expanding relationships.", "Our vision for RedCat Multiverse is that all citizens will  learn about themselves, others, patterns, motives, metaphors, motifs, relationships, skills, science, math, art, history, coding, languages, technology all inside a storytelling engine."],
-  },
-  {
-    id: '2',
-    title: "RCM LABS",
-    numeration: "02",
-    label: "It all started with one question: What if we could harness the engagement potential of blockchain gaming, but also encourage people to learn skills that go beyond playing games and make them more employable?",
-    column: "column_2",
-    column_m: "column_2",
-    item: 'item_c',
-    text: [
-      `It all started with one question: What if we could harness the engagement potential of blockchain gaming, but also encourage people to learn skills that go beyond playing games and make them more employable?`,
-      `In the Summer of 2021, we merged our 2019 research findings on soft skills with blockchain gaming and this is how the RedCat Multiverse was born with the world’s first Play- Learn - Earn model.`,
-      `RedCat Multiverse is a highly engaging blockchain game that provides socio - emotional activities, building skill mastery and expanding relationships.`,
-      `
-      Our vision for RedCat Multiverse is that all citizens will  learn about themselves, others, patterns, motives, metaphors, motifs, relationships, skills, science, math, art, history, coding, languages, technology all inside a storytelling engine.
-      `
-    ],
-  },
-  {
-    id: '3',
-    title: "PRESS",
-    numeration: "06",
-    label: "The Red Cat Multiverse is in the media!",
-    column: "column_2",
-    column_m: "column_2",
-    item: 'item_d',
-    text: ['The Red Cat Multiverse is in the media!'],
-  },
-  {
-    id: '4',
-    title: "MANIFESTO",
-    numeration: "03",
-    label: "Let's learn, grow, and dream…together!",
-    column: "column_3",
-    column_m: "column_1",
-    item: 'item_e',
-    text: [
-      'A brand new journey starts…',
-      'Here’s to the curious ones, those who love challenges, the inquisitive early adopters, with a never ending desire to empower the huma race…',
-      'Here’s to the crazy ones, those who think and feel beyond the present moment, the outliers who see things differently…',
-      'Here’s to the game changers who, as Steve Jobs said, “stay hungry, stay foolish…”',
-      'Meet Sanura, the RedCat.Together, you will tear down antiquated walls, and reimagine a better future!',
-      'Together, we can build more immersive experiences!',
-      'Let’s learn, grow, and dream…together!',
-    ],
-  },
-  {
-    id: '5',
-    title: "RCM INSIDERS",
-    numeration: "04",
-    label: "Help build the multiverse with us!",
-    column: "column_3",
-    column_m: "column_1",
-    item: 'item_f',
-    text: [`MEERKAT BAG NFTS`,
-      `
-            Inspired by the Loot project, Meerkat Bag NFTs are utility NFTs which provide building block primitives, upon which a foundational asset class can be built within the RedCat Multiverse.Meerkat Bags provide an opportunity to start with the content, before moving to gameplay, which in turn creates a purpose- driven community, rather than a product.Meerkat Bags are a collection of 888 NFTs, which have a unique combination of four character traits such as power skills, positive personality traits, even personality traits, and quirky traits.`
-      ,
-      `
-            Meerkat Bags essentially provide a prompt or building block primitive - upon which a foundational Multiverse asset Genius can be built.Meerkat Bags are exclusively created for hyper social RedCat Multiverse community members. 
-            `,
-      `RCM FELLOWS`,
-      `
-              We are also building tools to bridge the gap between the real world as well as existing Web 2.0 platforms like YouTube and Web 3.0 platforms.RCM Insiders who substantially contribute to multiverse development will become eligible to be RCM Fellows, important members who will be responsible for shaping this engagement and potentially get compensated for their contribution.,
-          `]
-  },
-  {
-    id: '6',
-    title: "TEAM",
-    numeration: "05",
-    label: "Here’s our team of the curious and crazy game changers.",
-    column: "column_3",
-    column_m: "column_2",
-    item: 'item_g',
-    text: "",
-  },
-];
-
+import { contents } from './constants';
 
 const CommunityGrid = () => {
 
@@ -197,7 +99,6 @@ const CommunityGrid = () => {
       onComplete: onComplete
     };
 
-
     // // capture state
     // const state = Flip.getState(fromHero, toHero);
 
@@ -207,8 +108,6 @@ const CommunityGrid = () => {
     //   ease: "power1.inOut",
     //   absolute: true
     // });
-
-
 
     gsap.from(clone, styleEnd);
 
---
+++ src/components/CommunityGrid/constants.ts
@@ -0,0 +1,97 @@
+export const contents = [
+  {
+    id: '0',
+    title: "TEAM",
+    numeration: "01",
+    label: "Here’s our team of the curious and crazy game changers.",
+    column: "column_1",
+    item: 'item_e',
+    text: [
+      'A brand new journey starts…',
+      'Here’s to the curious ones, those who love challenges, the inquisitive early adopters, with a never ending desire to empower the huma race…',
+      'Here’s to the crazy ones, those who think and feel beyond the present moment, the outliers who see things differently…',
+      'Here’s to the game changers who, as Steve Jobs said, “stay hungry, stay foolish…”',
+      'Meet Sanura, the RedCat.Together, you will tear down antiquated walls, and reimagine a better future!',
+      'Together, we can build more immersive experiences!',
+      'Let’s learn, grow, and dream…together!',
+    ],
+    image: "./images/community/community_01.png"
+  },
+  {
+    id: '6',
+    title: "UPDATES",
+    numeration: "06",
+    label: "Keep up on the latest in the Red Cat Multiverse",
+    column: "column_1",
+    item: 'item_f',
+    text: [`MEERKAT BAG NFTS`,
+      `
+            Inspired by the Loot project, Meerkat Bag NFTs are utility NFTs which provide building block primitives, upon which a foundational asset class can be built within the RedCat Multiverse.Meerkat Bags provide an opportunity to start with the content, before moving to gameplay, which in turn creates a purpose- driven community, rather than a product.Meerkat Bags are a collection of 888 NFTs, which have a unique combination of four character traits such as power skills, positive personality traits, even personality traits, and quirky traits.`
+      ,
+      `
+            Meerkat Bags essentially provide a prompt or building block primitive - upon which a foundational Multiverse asset Genius can be built.Meerkat Bags are exclusively created for hyper social RedCat Multiverse community members. 
+            `,
+      `RCM FELLOWS`,
+      `
+              We are also building tools to bridge the gap between the real world as well as existing Web 2.0 platforms like YouTube and Web 3.0 platforms.RCM Insiders who substantially contribute to multiverse development will become eligible to be RCM Fellows, important members who will be responsible for shaping this engagement and potentially get compensated for their contribution.,
+          `],
+    image: "./images/community/community_06.png"
+  },
+  {
+    id: '7',
+    title: "PRESS",
+    numeration: "07",
+    label: "The Red Cat Multiverse is in the media!",
+    column: "column_1",
+    item: 'item_g',
+    text: "",
+    image: "./images/community/community_07.png"
+  },
+  {
+    id: '2',
+    title: "WHITEPAPER",
+    numeration: "02",
+    label: "Want to know more about what we’re doing?",
+    column: "column_2",
+    item: 'item_a',
+    text: "",
+    image: "./images/community/community_02.png"
+  },
+  {
+    id: '5',
+    title: "MANIFESTO",
+    numeration: "05",
+    label: "Let's learn, grow, and dream…together!",
+    column: "column_2",
+    item: 'item_b',
+    text: ["It all started with one question: What if we could harness the engagement potential of blockchain gaming, but also encourage people to learn skills that go beyond playing games and make them more employable?", "In the Summer of 2021, we merged our 2019 research findings on soft skills with blockchain gaming and this is how the RedCat Multiverse was born with the world’s first Play-Learn-Earn model.", "RedCat Multiverse is a highly engaging blockchain game that provides socio - emotional activities, building skill mastery and expanding relationships.", "Our vision for RedCat Multiverse is that all citizens will  learn about themselves, others, patterns, motives, metaphors, motifs, relationships, skills, science, math, art, history, coding, languages, technology all inside a storytelling engine."],
+    image: "./images/community/community_05.png"
+  },
+  {
+    id: '3',
+    title: "RCM LABS",
+    numeration: "03",
+    label: "It all started with one question: What if we could harness the engagement potential of blockchain gaming, but also encourage people to learn skills that go beyond playing games and make them more employable?",
+    column: "column_3",
+    item: 'item_c',
+    text: [
+      `It all started with one question: What if we could harness the engagement potential of blockchain gaming, but also encourage people to learn skills that go beyond playing games and make them more employable?`,
+      `In the Summer of 2021, we merged our 2019 research findings on soft skills with blockchain gaming and this is how the RedCat Multiverse was born with the world’s first Play- Learn - Earn model.`,
+      `RedCat Multiverse is a highly engaging blockchain game that provides socio - emotional activities, building skill mastery and expanding relationships.`,
+      `
+      Our vision for RedCat Multiverse is that all citizens will  learn about themselves, others, patterns, motives, metaphors, motifs, relationships, skills, science, math, art, history, coding, languages, technology all inside a storytelling engine.
+      `
+    ],
+    image: "./images/community/community_03.png"
+  },
+  {
+    id: '4',
+    title: "RCM INSIDERS",
+    numeration: "04",
+    label: "Let's build the RedCat Multiverse together!",
+    column: "column_3",
+    item: 'item_d',
+    text: ['The Red Cat Multiverse is in the media!'],
+    image: "./images/community/community_04.png"
+  },
+];
