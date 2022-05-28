91e0ccc00619a9c2fd3758ca3bff793659b5c3c9
snowwhitedev <snowwhite91924@gmail.com>
2022-05-28 21:06:33 +0700

    feat: change lore to multiverse

---
+++ pages/icnft.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+import GradientText from '../src/components/gradientText/GradientText'
+import Container from '../src/components/container/Container'
+import CommunityGrid from '../src/components/CommunityGrid/CommunityGrid'
+import { useTheme } from 'next-themes'
+import styles from '../src/styles/community.module.scss'
+
+const ICNFT: NextPage = () => {
+  const { theme, systemTheme } = useTheme()
+  const currentTheme = theme === "system" ? systemTheme : theme
+  return (
+    <div>
+      <Container className={styles.container}>
+        <GradientText
+          element='h1'
+          text='NFT COLLECTION'
+          innerColor={currentTheme === 'dark' ? '#FFF' : '#1b1628'}
+          gradient={['#f9c930', '#f2957c', '#7192f3']}
+        />
+        <CommunityGrid />
+      </Container>
+    </div>
+  )
+}
+
+export default ICNFT;
