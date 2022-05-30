3f1aee8ecd1d0316c8efa23e9f0a9bed6d9254ef
snowwhitedev <snowwhite91924@gmail.com>
2022-05-30 15:02:43 +0700

    feat: added theme switch in mobile nav

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,26 +5,26 @@ import { themeChanger } from './helpers/themeChanger';
 import { NavMobile } from './NavMobile';
 import { NavDesktop } from './components/NavDesktop';
 
-const links = ['Community', 'Multiverse', 'Whitepaper', 'Roadmap', "ICNFT", "PLE", 'Careers']
+const links = ['Community', 'Multiverse', 'Whitepaper', 'Roadmap', "Inception NFT", "Play-Learn-Earn", 'Careers']
 
 const Header = () => {
 
-    const { theme } = themeChanger();
+  const { theme } = themeChanger();
 
-    return (
-        <Container className={styles.headerContainer}>
-            <header className={styles.header}>
-                {
-                    theme != 'dark'
-                        ? (<a href="/community"><img src='./images/rcm-logo-red.svg' alt='RCM Logo' /></a>)
-                        : (<a href="/community"><img src='./images/rcm-logo-white.svg' alt='RCM Logo' /></a>)
-                }
-                <NavDesktop links={links} />
-                <NavMobile links={links} />
+  return (
+    <Container className={styles.headerContainer}>
+      <header className={styles.header}>
+        {
+          theme != 'dark'
+            ? (<a href="/community"><img src='./images/rcm-logo-red.svg' alt='RCM Logo' /></a>)
+            : (<a href="/community"><img src='./images/rcm-logo-white.svg' alt='RCM Logo' /></a>)
+        }
+        <NavDesktop links={links} />
+        <NavMobile links={links} />
 
-            </header>
-        </Container>
-    )
+      </header>
+    </Container>
+  )
 }
 
-export default Header
+export default Header;
