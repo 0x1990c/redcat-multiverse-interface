bf12e5b9ddd8eae9cfc2b6c69fb4caf90ce08810
snowwhitedev <snowwhite91924@gmail.com>
2022-06-02 00:20:34 +0700

    feat: careers page

--- src/components/Careers/components/CareersCards.tsx
+++ src/components/Careers/components/CareersCards.tsx
@@ -3,14 +3,19 @@ import styles from '../careersComponents.module.scss'
 import cx from 'classnames';
 
 export const CareersCards = ({ card, index }: any) => {
-	const { border, colorText, category, title, description, button } = card
+	const { border, colorText, category, title, description, button, external } = card;
+
+	const handleClick = () => {
+		window.open('https://redcatmultiverse.substack.com/', '_blank');
+	}
+
 	return (
 		<div className={cx(styles.card, styles[border])} key={index}>
 			<h2 className={cx(styles.category, styles[colorText])}> {category.toUpperCase()} </h2>
 			<h1 className={styles.title}> {title.toUpperCase()} </h1>
 			<p className={styles.description}> {description} </p>
 			<div className={styles['container-button']}>
-				<button className={styles.button}> {button} </button>
+				<button className={styles.button} onClick={handleClick}> {button} </button>
 			</div>
 		</div>
 	)
