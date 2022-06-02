6897511ebaeb02770abc75e878a71b09a4958962
snowwhitedev <snowwhite91924@gmail.com>
2022-06-03 00:29:33 +0700

    feat: subscribe

--- src/components/ConnectModal/ConnectModal.tsx
+++ src/components/ConnectModal/ConnectModal.tsx
@@ -32,42 +32,58 @@ const ConnectModal = ({ onClose }: any) => {
   }
 
   const handleClick = async () => {
-    try {
 
-    } catch (error) {
-      console.log('error =>', error);
-    }
-    console.log('subscribe xxx', process.env.CC_APIKEY, process.env.CC_ACCESSTOKEN, process.env.CC_RCM_LIST_ID);
-    const subscribeData = {
-      lists: [
-        {
-          id: `${process.env.CC_RCM_LIST_ID}`
-        }
-      ],
-      email_addresses: [
-        {
-          "email_address": "hdl@example.com"
-        }
-      ]
-    }
-    const res = await fetch(`https://api.constantcontact.com/v2/contacts?action_by=ACTION_BY_OWNER&api_key=${process.env.CC_APIKEY}`, {
-      headers: {
-        "Content-Type": "application/json",
-        'Authorization': `Bearer ${process.env.CC_ACCESSTOKEN}`,
-        'Access-Control-Allow-Origin': "*",
-        'Access-Control-Allow-Headers': '*'
-      },
-      method: 'POST',
-      body: JSON.stringify(subscribeData)
-    });
 
-    console.log('res', res);
+    // try {
+
+    // } catch (error) {
+    //   console.log('error =>', error);
+    // }
+    // console.log('subscribe xxx', process.env.CC_APIKEY, process.env.CC_ACCESSTOKEN, process.env.CC_RCM_LIST_ID);
+    // const subscribeData = {
+    //   lists: [
+    //     {
+    //       id: `${process.env.CC_RCM_LIST_ID}`
+    //     }
+    //   ],
+    //   email_addresses: [
+    //     {
+    //       "email_address": "hcccdl@example.com"
+    //     }
+    //   ]
+    // }
+    // const res = await fetch(`https://api.constantcontact.com/v2/contacts?action_by=ACTION_BY_OWNER&api_key=${process.env.CC_APIKEY}`, {
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     'Authorization': `Bearer ${process.env.CC_ACCESSTOKEN}`,
+    //     'Access-Control-Allow-Origin': "*",
+    //     'Access-Control-Allow-Headers': '*'
+    //   },
+    //   method: 'POST',
+    //   body: JSON.stringify(subscribeData)
+    // });
+
+    // console.log('res', res);
 
     // const { error } = await res.json();
     // if (error) {
     //   console.log('error ==>', error);
     //   return;
     // }
+
+    const res = await fetch("/api/subscribe", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+
+
+    const { error } = await res.json();
+    if (error) {
+      console.log(error);
+      return;
+    }
   }
 
   return (
