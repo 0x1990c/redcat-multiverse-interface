0083854de1a6e264ae1fef6892e831febbf934ef
snowwhitedev <snowwhite91924@gmail.com>
2022-06-10 20:58:46 +0700

    feat: careers page button misalign && proofread

--- src/components/Careers/components/CareersCards.tsx
+++ src/components/Careers/components/CareersCards.tsx
@@ -11,9 +11,11 @@ export const CareersCards = ({ card, index }: any) => {
 
 	return (
 		<div className={cx(styles.card, styles[border])} key={index}>
-			<h2 className={cx(styles.category, styles[colorText])}> {category.toUpperCase()} </h2>
-			<h1 className={styles.title}> {title.toUpperCase()} </h1>
-			<p className={styles.description}> {description} </p>
+			<div>
+				<h2 className={cx(styles.category, styles[colorText])}> {category.toUpperCase()} </h2>
+				<h1 className={styles.title}> {title.toUpperCase()} </h1>
+				<p className={styles.description}> {description} </p>
+			</div>
 			<div className={styles['container-button']}>
 				<button className={styles.button} onClick={handleClick}> {button} </button>
 			</div>
