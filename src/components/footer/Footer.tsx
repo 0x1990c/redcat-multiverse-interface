619c0114c69721db7ea1b7112af115a5222fb5e8
snowwhitedev <snowwhite91924@gmail.com>
2022-06-21 00:44:52 +0700

    feat: added google analytics

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div>&#9993;&nbsp;<a href='mailto:hello@rcmlabs.io'>hello@rcmlabs.io</a></div>
+      <div className={styles.separator}>|</div>
       <div>© 2022 RCM Labs. All rights reserved.</div>
     </footer>
   )
