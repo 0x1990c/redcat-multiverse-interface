b1cb0df37cc5f0f385e5f6d700f26638d38c64a6
snowwhitedev <snowwhite91924@gmail.com>
2022-08-18 18:40:31 +0700

    feat: chore

--- src/components/header/components/NavMobileLinks.tsx
+++ src/components/header/components/NavMobileLinks.tsx
@@ -84,7 +84,7 @@ export const NavMobileLinks = ({ links, onOpenConnectModal, onCloseNav }: any) =
             onClick: () => setIsLangExpanded((x) => !x)
           })}
         >
-          LanguageA
+          Language
           <div className={`${styles.arrowWrapper} ${isLangExpanded ? styles.expanded : ''}`}>
             <ArrowDown />
           </div>
